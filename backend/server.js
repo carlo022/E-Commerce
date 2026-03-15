4f7a654b6e943b4f685d13b605642d13cd8352d1
carlo <carlolalas4@gmail.com>
2026-03-15 17:53:55 +0800

    fixing backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
-import { require } from 'express';
-
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 
 const server = jsonServer.create();
-const router = jsonServer.router('backend/data/db.json');
+const router = jsonServer.router('data/db.json');
 const middlewares = jsonServer.defaults();
 
 // Bind the router db to the app
@@ -15,7 +13,7 @@ server.use(middlewares);
 server.use(auth);
 
 // Add custom routes
-server.use(jsonServer.rewriter(require('backend/data/routes.json')));
+server.use(jsonServer.rewriter(require('./data/routes.json')));
 
 // Use default router
 server.use(router);

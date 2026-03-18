114617f9d785cfd38e59c1f576991a3f8f3f58e0
carlo <carlolalas4@gmail.com>
2026-03-18 23:58:32 +0800

    add vercel link

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  origin: ['https://e-commerce-lovat-three.vercel.app/', 'http://localhost:5000'],
   credentials: true
 }));
 app.use(express.json());

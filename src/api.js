58c79297aefcfbdf5909c784e3fdb8724049619d
carlo <carlolalas4@gmail.com>
2026-03-20 13:06:10 +0800

    resetting the front-end

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://e-commerce-production-c777.up.railway.app" || "http://localhost:8000";
+export const API_URL = "http://localhost:8000";
 
 export const fetchData = async (resource, query = "") => {
   const response = await fetch(`${API_URL}/${resource}?q=${query}`);
